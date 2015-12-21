a35bc6346d08db57fa61f4b27589a27c0c5dfad8
Martin Mahner <martin@mahner.org>
2015-12-21 22:53:44 +0100

    Apparently Wagtail requires compressor. Wut.

--- djde/settings/base.py
+++ djde/settings/base.py
@@ -131,6 +131,7 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
 )
 
 STATIC_URL = '/static/'

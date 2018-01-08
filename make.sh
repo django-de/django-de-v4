5888e0761e17d4d30c6355ae4af5d01e6add1d4b
Stephan Jaekel <steph@rdev.info>
2018-01-08 15:34:37 +0100

    Remove old make.sh.

--- make.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-mkdir -p build/css
-mkdir -p build/js
-mkdir -p build && rm -f build/assets && ln -s ../client/assets build/assets
-
-cat client/scss/base.scss | ./node_modules/.bin/node-sass --include-path client/scss > build/css/base.css
-./node_modules/.bin/browserify -t babelify --presets es2015 client/js/base.js > build/js/base.js
-

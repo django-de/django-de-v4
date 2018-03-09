ab0ea8536bf984c0b298b697c18b0e070a5a6302
Martin Mahner <martin@mahner.org>
2018-03-09 14:25:08 +0100

    Uploads folder

--- djangode/conf/prod_settings.py
+++ djangode/conf/prod_settings.py
@@ -10,11 +10,16 @@ MANAGERS = ADMINS = (
     ('Django e.V.', os.environ.get('ADMIN_EMAIL')),
 )
 
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
+
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 MEDIA_ROOT = os.environ.get('MEDIA_ROOT')
 STATIC_ROOT = os.environ.get('STATIC_ROOT')
+MEDIA_URL = '/uploads/'
+STATIC_URL = '/static/'
 
 # Sentry
 INSTALLED_APPS += ('raven.contrib.django.raven_compat',)
